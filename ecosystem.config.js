3c367ef13dbdc0d06821838d2b812a0b964ab6ec
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2024-07-26 23:49:09 -0500

    🎉 usar ecosistema para pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: 'cara',
+      script: 'serve',
+      env: {
+        PM2_SERVE_PATH: './aplicaciones/cara/publico',
+        PM2_SERVE_PORT: 4001,
+      },
+    },
+
+    {
+      name: 'culo',
+      script: './aplicaciones/culo/publico/programa.js',
+    },
+  ],
+};
